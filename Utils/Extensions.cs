f67581fef0f44aa99414486cbb884e21b1fa0efb
Sina MohammadZadeh <sina75mn@outlook.com>
2025-02-18 15:29:13 +0330

    add string extensions

--- Utils/Extensions.cs
+++ Utils/Extensions.cs
@@ -3,5 +3,9 @@ namespace U.Utils;
 public static class StringExtension {
 	public static bool IsNotNullOrEmpty(this string? s) => s is { Length: > 0 };
 
+	public static bool IsNotNull(this string? s) => s != null;
+
 	public static bool IsNullOrEmpty(this string? s) => string.IsNullOrEmpty(s);
+
+	public static bool IsNull(this string? s) => s == null;
 }
