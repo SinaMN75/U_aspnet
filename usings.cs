f1b1ce68005149a13ffbe3e7d2ec3d85a89b3193
Sina MohammadZadeh <sina75mn@outlook.com>
2025-02-19 21:02:02 +0330

    removed global usings

--- usings.cs
+++
@@ -1,7 +0,0 @@
-global using U.Utils;
-global using U;
-global using System.Text.Json;
-global using System.Text.Json.Serialization;
-global using System.ComponentModel.DataAnnotations;
-global using Microsoft.Extensions.Caching.Distributed;
-global using Microsoft.EntityFrameworkCore;
