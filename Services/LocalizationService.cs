dcd3b94f1d758f312177e0ae76e092e85ca26e9d
Sina MohammadZadeh <sina75mn@outlook.com>
2025-02-25 15:48:45 +0330

    :)

--- Services/LocalizationService.cs
+++ Services/LocalizationService.cs
@@ -1,27 +1,18 @@
+using Microsoft.AspNetCore.Http;
+
 namespace U.Services;
 
 using System.Text.Json;
 
 public interface ILocalizationService {
-	string GetMessage(string locale, string key);
+	string GetMessage(string key, string? locale = null);
 }
 
-public class LocalizationService : ILocalizationService {
-	private readonly Dictionary<string, Dictionary<string, string>> _localizedMessages;
+public class LocalizationService(IHttpContextAccessor httpContext) : ILocalizationService {
+	private readonly Dictionary<string, Dictionary<string, string>> _localizedMessages = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(File.ReadAllText("LocalizedMessages.json")) ?? new Dictionary<string, Dictionary<string, string>>();
 
-	public LocalizationService() {
-		try {
-			string json = File.ReadAllText("LocalizedMessages.json");
-			_localizedMessages = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(json) ?? new Dictionary<string, Dictionary<string, string>>();
-		}
-		catch (Exception ex) {
-			Console.WriteLine($"Error loading localization file: {ex.Message}");
-			_localizedMessages = new Dictionary<string, Dictionary<string, string>>();
-		}
-	}
-
-	public string GetMessage(string locale, string key) {
-		return _localizedMessages.TryGetValue(locale, out Dictionary<string, string>? messages) && messages.TryGetValue(key, out string? message)
+	public string GetMessage(string key, string? locale) {
+		return _localizedMessages.TryGetValue(locale ?? httpContext.HttpContext?.Request.Headers["Locale"].FirstOrDefault() ?? "en", out Dictionary<string, string>? messages) && messages.TryGetValue(key, out string? message)
 			? message
 			: _localizedMessages.GetValueOrDefault("en")?.GetValueOrDefault(key, "Message not found") ?? "Message not found";
 	}
