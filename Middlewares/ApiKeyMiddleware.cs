56a4ac226891601efa5472f2ad0fa65c864f54a8
Sina MohammadZadeh <sina75mn@outlook.com>
2025-02-26 13:23:01 +0330

    :)

--- Middlewares/ApiKeyMiddleware.cs
+++ Middlewares/ApiKeyMiddleware.cs
@@ -5,22 +5,24 @@ namespace U.Middlewares;
 
 public class ApiKeyMiddleware(RequestDelegate next, string apiKey) {
 	public async Task InvokeAsync(HttpContext context) {
-		if (!context.Request.ContentType?.Contains("application/json") ?? true) {
-			context.Response.StatusCode = StatusCodes.Status400BadRequest;
-			await context.Response.WriteAsync("Invalid content type");
-			return;
-		}
+		if (context.Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase)) {
+			if (!context.Request.ContentType?.Contains("application/json") ?? true) {
+				context.Response.StatusCode = StatusCodes.Status400BadRequest;
+				await context.Response.WriteAsync("Invalid content type");
+				return;
+			}
 
-		context.Request.EnableBuffering();
-		string body = await new StreamReader(context.Request.Body).ReadToEndAsync();
-		context.Request.Body.Position = 0;
+			context.Request.EnableBuffering();
+			string body = await new StreamReader(context.Request.Body).ReadToEndAsync();
+			context.Request.Body.Position = 0;
 
-		JsonElement json = JsonSerializer.Deserialize<JsonElement>(body);
-		if (!json.TryGetProperty("apiKey", out JsonElement apiKeyProperty) ||
-		    apiKeyProperty.GetString() != apiKey) {
-			context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-			await context.Response.WriteAsync("Invalid API key");
-			return;
+			JsonElement json = JsonSerializer.Deserialize<JsonElement>(body);
+			if (!json.TryGetProperty("apiKey", out JsonElement apiKeyProperty) ||
+			    apiKeyProperty.GetString() != apiKey) {
+				context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+				await context.Response.WriteAsync("Invalid API key");
+				return;
+			}
 		}
 
 		await next(context);
